b12d28d97321144c41979febe52851b74bd7c7de
chaitanya_krt <tck.16051889@gmail.com>
2024-09-21 02:09:32 +0530

    Docker updated

---
+++ server/db/mondb.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const DBConnection = async () => {
+    const MONGO_URI = 'mongodb+srv://admin123:Czd1M0nCANXlqCgU@cluster0.3komplc.mongodb.net/?retryWrites=true&w=majority';
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+        console.log("DB connected successfully");
+    } catch (error) {
+        console.log("Error while connecting to MongoDB", error.message);
+    }
+};
+
+module.exports = {
+    DBConnection, 
+}
---
+++ server/db/mongodb.js
@@ -0,0 +1,21 @@
+const DBConnection = require('./mondb.js');
+const mongoose = require('mongoose');
+DBConnection.DBConnection();
+
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports = {
+    collection,
+}
+
